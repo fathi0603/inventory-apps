f998d5e25cade19e03a485333b190f7feaa26e01
hafidz fathi azkia <hafidzfathiazkia@hafidzs-MacBook-Air.local>
2026-08-12 03:26:52 +0700

    Redirect homepage to login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 Route::get('/barang', [BarangController::class, 'index']);
